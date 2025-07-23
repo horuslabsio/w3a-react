b366138f5ef8b5d60d90931f7db02dfd0b65ebf7
Stephanie Egbuonu <100955511+stephanniegb@users.noreply.github.com>
2025-07-23 17:14:28 +0100

    Rename useWeb3AuthUser hook and bump version

--- src/hooks/use-web3auth-user.ts
+++ src/hooks/use-web3auth-user.ts
@@ -1,11 +1,17 @@
-import { useWeb3AuthUser } from "@web3auth/modal/react";
+import { useWeb3AuthUser as _useWeb3AuthUser } from "@web3auth/modal/react";
 /**
  * Custom hook that wraps the Web3Auth user hook and provides additional functionality
  * @returns Object containing user state and functions
  */
-export const useWeb3AuthUserHook = () => {
+export const useWeb3AuthUser = (): {
+  loading: boolean;
+  error: any;
+  userInfo: any;
+  isMFAEnabled: boolean;
+  getUserInfo: () => Promise<any>;
+} => {
   const { loading, error, userInfo, isMFAEnabled, getUserInfo } =
-    useWeb3AuthUser();
+    _useWeb3AuthUser();
 
   return {
     loading,
